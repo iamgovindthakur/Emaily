5ca194d4d5ea1246fa8faa80c405b8de153ef6ed
Govind Kumar Thakur <iamgovindthakur@gmail.com>
2020-10-13 13:05:05 +0530

    Emaily Survey App

---
+++ config/dev.js
@@ -0,0 +1,15 @@
+module.exports = {
+  googleClientID:
+    "292227748666-529e351mlgophso9srgbhm85q2t9n56q.apps.googleusercontent.com",
+  googleClientSecret: "0couzwfWfJHwFejc-E9Yx1iw",
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0.9s8lu.mongodb.net/Emaily?retryWrites=true&w=majority",
+  cookieKey: "hcbbdjhbdfjknsgfvhfvhfdetycgeyubdgcvyuhbdhvdhybsvcbs",
+  stripePublishableKey:
+    "pk_test_51HZytZGeFblkAxS0rbQIDPLTmmWOTcrnEiwKmpaORUVqr7jWHJNegUDdYYxPOALkcm9zOJKc0mpTMauZVkmLgVwC000KHEUkr1",
+  stripeSecretKey:
+    "sk_test_51HZytZGeFblkAxS0Er7lxXF47aCVUhKKYwaW2SvbcD3oHnCaNbwqHFXIBMbEZMTWQudm8f7IYq8hreaqVhztMIbW00oxN4qLw8",
+    sendGridKey:'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',
+    redirectDomain: 'http://localhost:5000'
+  
+};
